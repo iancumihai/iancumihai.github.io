fa4c6165cd78d84a31b6ea721c1ff6704001e881
iancumihai <iancu_mihai24@yahoo.com>
2017-10-18 19:34:27 +0300

    rotunjire la 2 cifre

--- sensors/sensors.js
+++ sensors/sensors.js
@@ -3,9 +3,9 @@ window.addEventListener('deviceorientation', ondeviceorientation);
 //---------------------------------------------------------------
 function ondeviceorientation(event) 
 { 
-	document.getElementById("id_alpha").innerHTML = event.alpha;
-	document.getElementById("id_beta").innerHTML = event.beta;
-	document.getElementById("id_gamma").innerHTML = event.gamma;
+	document.getElementById("id_alpha").innerHTML = Math.rand(event.alpha * 100) / 100;
+	document.getElementById("id_beta").innerHTML = Math.rand(event.beta * 100) / 100;
+	document.getElementById("id_gamma").innerHTML = Math.rand(event.gamma * 100) / 100;
 }
 
 //---------------------------------------------------------------
