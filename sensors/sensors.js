6441f0f13e24d0e074e6c75851349e4e14ec6388
iancumihai <iancu_mihai24@yahoo.com>
2017-10-18 19:35:51 +0300

    rotunjire la 2

--- sensors/sensors.js
+++ sensors/sensors.js
@@ -3,9 +3,9 @@ window.addEventListener('deviceorientation', ondeviceorientation);
 //---------------------------------------------------------------
 function ondeviceorientation(event) 
 { 
-	document.getElementById("id_alpha").innerHTML = Math.rand(event.alpha * 100) / 100;
-	document.getElementById("id_beta").innerHTML = Math.rand(event.beta * 100) / 100;
-	document.getElementById("id_gamma").innerHTML = Math.rand(event.gamma * 100) / 100;
+	document.getElementById("id_alpha").innerHTML = Math.round(event.alpha * 100) / 100;
+	document.getElementById("id_beta").innerHTML = Math.round(event.beta * 100) / 100;
+	document.getElementById("id_gamma").innerHTML = Math.round(event.gamma * 100) / 100;
 }
 
 //---------------------------------------------------------------
