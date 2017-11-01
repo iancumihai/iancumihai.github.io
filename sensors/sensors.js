1b887992ab72582d8649ce9b3dafb2b361a97912
iancumihai <iancu_mihai24@yahoo.com>
2017-11-01 18:25:15 +0200

    am adugat ceva

--- sensors/sensors.js
+++ sensors/sensors.js
@@ -24,6 +24,7 @@ function deseneaza_cerc(gamma, beta)
 
 	context.stroke();
 }
+//---------------------------------------------------------------
 function ondeviceorientation(event) 
 { 
 	var alpha = event.alpha;
