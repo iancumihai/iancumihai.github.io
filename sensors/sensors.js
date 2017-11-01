4025623b4c9b53d6339850f08558435a5e72d25f
iancumihai <iancu_mihai24@yahoo.com>
2017-11-01 19:19:40 +0200

    desenare patrat

--- sensors/sensors.js
+++ sensors/sensors.js
@@ -3,7 +3,29 @@ window.addEventListener('deviceorientation', ondeviceorientation);
 
 window.addEventListener('devicemotion', ondevicemotion);
 
+function deseneaza_patrat_canvas(alpha, beta, gamma)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
 
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+
+	context.clearRect(0, 0, h, w);
+
+	var centru = {x:w / 2, y:h / 2};
+	var raza = 10;
+	var max_deplasare_x = w / 2 - raza;
+	var max_deplasare_y = h / 2 - raza;
+	// gamma == 0 -> centru
+	// gamma == -90 -> 0 + raza
+	// gamma == 90 -> w - raza
+	context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, raza, 0, 2 * Math.PI);
+
+	context.stroke();
+}
+//---------------------------------------------------------------
 function deseneaza_cerc_svg(gamma, beta)
 {	
 	var svg = document.getElementById("id_svg");
@@ -34,14 +56,12 @@ function deseneaza_cerc_canvas(gamma, beta)
 	context.clearRect(0, 0, h, w);
 
 	var centru = {x:w / 2, y:h / 2};
-	var raza = 10;
+	var latura = 10;
 	var max_deplasare_x = w / 2 - raza;
 	var max_deplasare_y = h / 2 - raza;
-	// gamma == 0 -> centru
-	// gamma == -90 -> 0 + raza
-	// gamma == 90 -> w - raza
-	context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, raza, 0, 2 * Math.PI);
-
+	
+	//context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, raza, 0, 2 * Math.PI);
+	context.strokeRect(centru.x - latura / 2 + gamma / 90 * max_deplasare_x, centru.y - latura / 2 + beta / 90 * max_deplasare_y, latura, latura);
 	context.stroke();
 }
 
@@ -57,7 +77,8 @@ function ondeviceorientation(event)
 	document.getElementById("id_gamma").innerHTML ="gama= " + Math.round(gamma * 10) / 10;
 
 	//deseneaza_cerc_canvas(gamma, beta);
-	deseneaza_cerc_svg(gamm, beta);
+	//deseneaza_cerc_svg(gamm, beta);
+	deseneaza_patrat_canvas(alpha, beta, gamma)
 }
 //---------------------------------------------------------------
 function ondevicemotion(event)
