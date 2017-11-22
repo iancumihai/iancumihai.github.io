3516006f026438091e8014d86a82a37abda471c6
iancumihai <iancu_mihai24@yahoo.com>
2017-11-22 18:06:00 +0200

    better position

--- sensor/canvas_move.js
+++ sensor/canvas_move.js
@@ -27,11 +27,13 @@ function on_touch_move(e)
 	for (var i=0; i < touches.length; i++) 
 	{
 		if(touches[i].pageX  - rect_canvas.left < top_x + img_width && touches[i].pageX - rect_canvas.left >= top_x &&
-			touches[i].pageY - rect_canvas.top < top_y + img_height && touches[i].pageY - rect_canvas.top >= top_y )
+			touches[i].pageY - rect_canvas.top < top_y + img_height && touches[i].pageY - rect_canvas.top >= top_y)
 			{
+				var offset_inside_image_x = top_x - (touches[i].pageX  - rect_canvas.left);
+				var offset_inside_image_y = top_y - (touches[i].pageY  - rect_canvas.top);
 				context.clearRect(0, 0, 400, 300);
-				top_x = touches[i].pageX - rect_canvas.left;
-				top_y = touches[i].pageY - rect_canvas.top;
+				top_x = touches[i].pageX - rect_canvas.left - offset_inside_image_x;
+				top_y = touches[i].pageY - rect_canvas.top - offset_inside_image_y;
 				context.drawImage(img, top_x, top_y, img_width, img_height);
 			}
 	}
