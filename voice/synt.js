e26b765b74fcd7a55cde98f6e0187c88b5c74b46
iancumihai <iancu_mihai24@yahoo.com>
2017-11-22 19:06:25 +0200

    voice synth

--- voice/synt.js
+++ voice/synt.js
@@ -1,6 +1,7 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.1";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.2";
 
 var synt = window.speechSynthesis;
+
 
 //---------------------------------------------
 function get_voices()
@@ -10,3 +11,9 @@ function get_voices()
 		document.getElementById("id_voices").innerHTML += voices[i].name + ":" + voices[i].lang + "<br>";
 }
 //---------------------------------------------
+function speak()
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.text = document.getElementById("id_text").value;
+	synt.speak(enunt);
+}
