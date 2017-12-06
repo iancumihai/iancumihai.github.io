c8457dd88d5e3c9b39f8dd40d6e1a8bfbc664ea1
iancumihai <iancu_mihai24@yahoo.com>
2017-12-06 17:37:18 +0200

    gps

---
+++ gps/gps.js
@@ -0,0 +1,17 @@
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.12.06.1"; 
+
+
+
+navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
+
+//-----------------------------------------------------------------------------
+function on_gps_success(p)
+{
+	document.getElementById("id_p").innerHTML = "lat = " + p.coords.latitude + " long = " + p.coords.longitude + "accuracy = " + p.coords.accuracy + "m" ;
+}
+//-----------------------------------------------------------------------------
+function on_gps_error(e)
+{
+	allert(e.code);
+}
+//-----------------------------------------------------------------------------
