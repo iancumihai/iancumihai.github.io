350102fd107675f0fda6616fe96e9f9a0c5d499a
iancumihai <iancu_mihai24@yahoo.com>
2017-12-06 18:17:35 +0200

    adaugat gps

--- js/gps.js
+++ js/gps.js
@@ -7,7 +7,15 @@ navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
 //-----------------------------------------------------------------------------
 function on_gps_success(p)
 {
-	document.getElementById("id_p").innerHTML = "lat = " + p.coords.latitude + "<br>long = " + p.coords.longitude + "<br>accuracy = " + p.coords.accuracy + "m" + "<br>altitude= " + p.coords.altitude + "m" ;
+	document.getElementById("id_p").innerHTML = 
+	"lat = " + p.coords.latitude + 
+	"<br>long = " + p.coords.longitude + 
+	"<br>accuracy = " + p.coords.accuracy + "m" + 
+	"<br>altitude= " + p.coords.altitude + "m" ;
+
+	var map_str = "https://maps.googleapis.com/maps/api/staticmap?markers=color:blue|"+p.coords.latitude+","+p.coords.longitude+"&zoom=15&size=320x240&key=AIzaSyAX-afGftQdoSsjJ20r40gdPpiRS7MMlag";
+
+	document.getElementById("id_gps_img").setAttribute("src", map_str);
 }
 //-----------------------------------------------------------------------------
 function on_gps_error(e)
