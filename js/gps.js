b4bac48c08c0a9fd10d5bb799cae660d98f086e2
iancumihai <iancu_mihai24@yahoo.com>
2017-12-06 18:41:00 +0200

    adaugat speed

--- js/gps.js
+++ js/gps.js
@@ -10,11 +10,16 @@ function on_gps_success(p)
 	document.getElementById("id_p").innerHTML = 
 	"lat = " + p.coords.latitude + 
 	"<br>long = " + p.coords.longitude + 
-	"<br>accuracy = " + p.coords.accuracy + "m" + 
-	"<br>altitude= " + p.coords.altitude + "m" ;
+	"<br>accuracy = " + p.coords.accuracy + " m" + 
+	"<br>altitude = " + p.coords.altitude + " m" + 
+	"<br>speed = " + p.coords.speed + " m/s";
 
-	var map_str = "https://maps.googleapis.com/maps/api/staticmap?markers=color:blue|"+p.coords.latitude+","+p.coords.longitude+"&zoom=15&size=320x240&key=AIzaSyAX-afGftQdoSsjJ20r40gdPpiRS7MMlag";
-
+	var map_str = "https://maps.googleapis.com/maps/api/staticmap?"+
+	"markers=color:blue|"+ p.coords.latitude + "," + p.coords.longitude + "|Palatul+Apor"+
+	"&path=color:0xff0000|" + p.coords.latitude + "," + p.coords.longitude + "|Palatul+Apor"+
+	"&zoom=15"+
+	"&size=320x240"+
+	"&key=AIzaSyAX-afGftQdoSsjJ20r40gdPpiRS7MMlag";
 	document.getElementById("id_gps_img").setAttribute("src", map_str);
 }
 //-----------------------------------------------------------------------------
