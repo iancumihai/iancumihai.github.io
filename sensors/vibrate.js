9ccaed915769dc88f08aa556e564f6b344fd92d6
iancumihai <iancu_mihai24@yahoo.com>
2018-01-10 19:12:18 +0200

    incrase business version

--- sensors/vibrate.js
+++ sensors/vibrate.js
@@ -1,7 +1,7 @@
 var d = new Date();
 document.getElementById("id_logic_level_version").innerHTML = 
 	"Business level version:" + 
-	d.getFullYear() + "." + d.getMonth() + "1" + "." + d.getDate()+".5";
+	d.getFullYear() + "." + d.getMonth() + "1" + "." + d.getDate()+".2";
 
 document.addEventListener("touchstart", vibrate);
 
